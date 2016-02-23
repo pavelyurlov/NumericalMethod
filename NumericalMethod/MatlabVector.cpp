59f3a2f42a917b852e41f7d63ce0a3b198e54311
gads06121994 <gads06121994@yandex.ru>
2016-02-23 20:10:49 +0300

    Убрал лямбда-функции

--- NumericalMethod/MatlabVector.cpp
+++ NumericalMethod/MatlabVector.cpp
@@ -2,9 +2,6 @@
 #include <string>
 
 
-// может, если возвращать копии, а не ссылки, то утечек не будет?
-// то есть MV вместо MV&
-
 MatlabVector MatlabVector::per_member_op(MatlabVector &other, num(*op)(num, num))
 {
 	if (this->size() != other.size()) throw new std::string("почленная операция над векторами разного размера недопустима");
@@ -22,7 +19,48 @@ MatlabVector MatlabVector::per_member_op(num a, num(*op)(num, num))
 
 MatlabVector operator*(num n, MatlabVector mv) { return mv*n; }
 
-MatlabVector operator+(MatlabVector a, MatlabVector b) { return a.operator_plus(b); }
-MatlabVector operator*(MatlabVector a, MatlabVector b) { return a.operator_mult(b); }
-MatlabVector operator-(MatlabVector a, MatlabVector b) { return a.operator_minus(b); }
-MatlabVector operator/(MatlabVector a, MatlabVector b) { return a.operator_frag(b); }
+MatlabVector operator+(MatlabVector a, MatlabVector b)
+{
+	if (a.size() != b.size()) throw new std::string("почленная операция над векторами разного размера недопустима");
+	MatlabVector result = MatlabVector(a.size());
+	for (uint i = 0; i < a.size(); i++) result[i] = a[i] + b[i];
+	return result;
+}
+
+MatlabVector operator-(MatlabVector a, MatlabVector b)
+{
+	if (a.size() != b.size()) throw new std::string("почленная операция над векторами разного размера недопустима");
+	MatlabVector result = MatlabVector(a.size());
+	for (uint i = 0; i < a.size(); i++) result[i] = a[i] - b[i];
+	return result;
+}
+
+MatlabVector operator*(MatlabVector a, MatlabVector b)
+{
+	if (a.size() != b.size()) throw new std::string("почленная операция над векторами разного размера недопустима");
+	MatlabVector result = MatlabVector(a.size());
+	for (uint i = 0; i < a.size(); i++) result[i] = a[i] * b[i];
+	return result;
+}
+
+MatlabVector operator/(MatlabVector a, MatlabVector b)
+{
+	if (a.size() != b.size()) throw new std::string("почленная операция над векторами разного размера недопустима");
+	MatlabVector result = MatlabVector(a.size());
+	for (uint i = 0; i < a.size(); i++) result[i] = a[i] / b[i];
+	return result;
+}
+
+MatlabVector MatlabVector::operator+(num a)
+{
+	MatlabVector result = MatlabVector(this->size());
+	for (uint i = 0; i < this->size(); i++) result[i] = (*this)[i] + a;
+	return result;
+}
+
+MatlabVector MatlabVector::operator*(num a)
+{
+	MatlabVector result = MatlabVector(this->size());
+	for (uint i = 0; i < this->size(); i++) result[i] = (*this)[i] * a;
+	return result;
+}
--- NumericalMethod/MatlabVector.h
+++ NumericalMethod/MatlabVector.h
@@ -10,12 +10,12 @@ public:
 	MatlabVector() {}
 	MatlabVector(uint N) :std::vector<num>(N) {} // garbage inside?
 	template <class InputIterator> MatlabVector(InputIterator first, InputIterator last) : vector<num>(first, last) {}
-	MatlabVector operator_plus(MatlabVector&o) { return per_member_op(o, [](num a, num b) {return a + b; }); } // matlab .+
-	MatlabVector operator_minus(MatlabVector&o) { return per_member_op(o, [](num a, num b) {return a - b; }); }
-	MatlabVector operator_frag(MatlabVector&o) { return per_member_op(o, [](num a, num b) {return a / b; }); }
-	MatlabVector operator_mult(MatlabVector&o) { return per_member_op(o, [](num a, num b) {return a * b; }); }
-	MatlabVector operator+(num a) { return per_member_op(a, [](num b, num c) {return b + c; }); }
-	MatlabVector operator*(num a) { return per_member_op(a, [](num b, num c) {return b * c; }); }
+	friend MatlabVector operator+(MatlabVector a, MatlabVector b);
+	friend MatlabVector operator-(MatlabVector a, MatlabVector b);
+	friend MatlabVector operator*(MatlabVector a, MatlabVector b);
+	friend MatlabVector operator/(MatlabVector a, MatlabVector b);
+	MatlabVector operator+(num a);
+	MatlabVector operator*(num a);
 private:
 	MatlabVector per_member_op(MatlabVector&, num(*op)(num, num));
 	MatlabVector per_member_op(num, num(*op)(num, num));
@@ -23,8 +23,5 @@ private:
 
 MatlabVector operator*(num n, MatlabVector mv);
 
-MatlabVector operator+(MatlabVector a, MatlabVector b);// { return a.operator_plus(b); }
-MatlabVector operator*(MatlabVector a, MatlabVector b);// { return a.operator_mult(b); }
-MatlabVector operator-(MatlabVector a, MatlabVector b);// { return a.operator_minus(b); }
-MatlabVector operator/(MatlabVector a, MatlabVector b);// { return a.operator_frag(b); }
-
+// 14.23 с лямбда-функциями
+// 13.05 без лямбда-функций
