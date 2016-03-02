1c915566b3473232a129a7c06891f3c3aa87f21d
gads06121994 <gads06121994@yandex.ru>
2016-03-03 00:36:54 +0300

    Улучшил makefile. Перенёс некоторый код с целью сохранения кроссплатформенности.

--- NumericalMethod/IOSets.h
+++ NumericalMethod/IOSets.h
@@ -25,14 +25,44 @@ struct InputSet
 	num d12;
 	num d21;
 	num d22;
-	operator Json::Value() const;
+	operator Json::Value() const
+	{
+		Json::Value root;
+		root["A"] = this->A;
+		root["N"] = this->N;
+		root["max_iter"] = this->max_iter;
+		root["a"] = this->a;
+		//root["f_***"] = this->f_m1***.rasterize(); //TODO
+		root["b1"] = this->b1;
+		root["b2"] = this->b2;
+		root["d1"] = this->d1;
+		root["d2"] = this->d2;
+		root["d11"] = this->d11;
+		root["d12"] = this->d12;
+		root["d22"] = this->d22;
+		return root;
+	}
 };
 
 struct OutputSet
 {
 	MatlabVector rh, D11, D12, D22;
 	num y11, y12, y21, y22, N1, N2;
-	operator Json::Value() const;
+	operator Json::Value() const
+	{
+		Json::Value root;
+		root["rh"] = this->rh;
+		root["D11"] = this->D11;
+		root["D12"] = this->D12;
+		root["D22"] = this->D22;
+		root["y11"] = this->y11;
+		root["y12"] = this->y12;
+		root["y21"] = this->y21;
+		root["y22"] = this->y22;
+		root["N1"] = this->N1;
+		root["N2"] = this->N2;
+		return root;
+	}
 };
 
 std::ostream& operator<<(std::ostream&, OutputSet&);
