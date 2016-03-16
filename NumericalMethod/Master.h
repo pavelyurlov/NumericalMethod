a634b5b66c11186a6308a65f2a22aa1dad6831f7
gads06121994 <gads06121994@yandex.ru>
2016-03-17 02:22:02 +0300

    Модифицировал вывод для получения валидного JSON-а.

--- NumericalMethod/Master.h
+++ NumericalMethod/Master.h
@@ -19,23 +19,20 @@ void count_input(std::vector<InputSet> input_array)
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
-	std::ostringstream filename;
-	filename << "proc_" << mpi_rank << "_" << mpi_size << ".txt";
-
-
-	std::ofstream ofs;
-	ofs.open(filename.str());
+	Json::Value root;
+	root = Json::arrayValue;
 
 	for (uint i = 0; i < input_array.size(); i++)
 	{
 		if (i % mpi_size == mpi_rank)
 		{
-			Json::Value root;
-			root["JobNumber"] = i;
-			root["Input"] = input_array[i];
-			root["Output"] = solve_iter_sym(input_array[i]);
-			ofs << root;
-			root.clear();
+			Json::Value newborn = Json::objectValue;
+			newborn["JobNumber"] = i;
+			newborn["Input"] = input_array[i];
+			newborn["Output"] = solve_iter_sym(input_array[i]);
+			//ofs << root;
+			root.append(newborn);
+			newborn.clear(); // too paranoic?
 
 			/*ofs << "Job #" << i << " with input:" << std::endl;
 			ofs << input_array[i] << std::endl;
@@ -45,5 +42,12 @@ void count_input(std::vector<InputSet> input_array)
 		}
 	}
 
+	std::ostringstream filename;
+	filename << "proc_" << mpi_rank << "_" << mpi_size << ".txt";
+
+	std::ofstream ofs;
+	ofs.open(filename.str());
+
+	ofs << root;
 	ofs.close();
 }
