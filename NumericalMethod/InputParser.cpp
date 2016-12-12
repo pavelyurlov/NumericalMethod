cc5352f4c5517e7afbef15b95c5a0186f900d3e6
gads06121994 <gads06121994@yandex.ru>
2016-12-12 18:01:43 +0300

    Продолжил выносить этот ввод.

--- NumericalMethod/InputParser.cpp
+++ NumericalMethod/InputParser.cpp
@@ -2,10 +2,66 @@
 #include <fstream>
 #include "json\json.h"
 
+template <typename T>
+void readFieldVersion1(Json::Value root, std::string fieldName, T &field)
+{
+	// TODO: Добавить обработку пунктов 8 и 9.
+
+	if (root.isMember(fieldName))
+	{
+		// А это вообще поддерживается языком?
+		switch (T)
+		{
+		case int:
+			field = root[fieldName].asInt();
+			break;
+		case float:
+			field = root[fieldName].asFloat();
+			break;
+		default:
+			// TODO: Кинуть исключение
+		}
+	}
+	else
+	{
+		// TODO: Кинуть исключение
+	}
+}
 
 std::vector<InputSet> parseVersion1(Json::Value root)
 {
-	///
+	// Для лучшего понимания смотри "Формат входного файла численного метода, версия 1"
+
+	// Проверяем, является ли файл ссылкой
+	if (root.isMember("InputFileName"))
+	{
+		// TODO: Проверить наличие других полей, если есть, кинуть исключение
+
+		std::string inputFileName = root["InputFileName"].asString();
+		return InputParser::Parse(inputFileName);
+	}
+
+	// Читаем поля
+	InputSet is;
+	readFieldVersion1(root, "SizeOfArea", is.A);
+	readFieldVersion1(root, "NumberOfGridDots", is.N);
+	readFieldVersion1(root, "ClosureConstantAlpha", is.a);
+	readFieldVersion1(root, "DeathRate1", is.d1);
+	readFieldVersion1(root, "DeathRate2", is.d2);
+	readFieldVersion1(root, "BirthRate1", is.b1);
+	readFieldVersion1(root, "BirthRate2", is.b2);
+	readFieldVersion1(root, "DeathRate1To1", is.d11);
+	readFieldVersion1(root, "DeathRate1To2", is.d12);
+	readFieldVersion1(root, "DeathRate2To1", is.d21);
+	readFieldVersion1(root, "DeathRate2To2", is.d22);
+	readFieldVersion1(root, "SigmaBirth1", is.sm1);
+	readFieldVersion1(root, "SigmaBirth2", is.sm2);
+	readFieldVersion1(root, "SigmaDeath1To1", is.sw11);
+	readFieldVersion1(root, "SigmaDeath1To2", is.sw12);
+	readFieldVersion1(root, "SigmaDeath2To1", is.sw21);
+	readFieldVersion1(root, "SigmaDeath2To2", is.sw22);
+
+	// TODO: Добавить чтение полей с настройками
 }
 
 std::vector<InputSet> InputParser::Parse(std::string filename)
@@ -22,15 +78,19 @@ std::vector<InputSet> InputParser::Parse(std::string filename)
 	file >> root;
 	int format = root["InputFileFormat"].asInt();
 
+	std::vector<InputSet> result;
+
 	// Читаем файл
 	switch (format)
 	{
 	case 1:
-		parseVersion1(root);
+		result = parseVersion1(root);
 		break;
-	default:
+	//sdefault:
 		// TODO: Бросить исключение о неизвестном формате
 	}
+
+	return result;
 
 
 
