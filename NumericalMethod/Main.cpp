b0f1447aaeb1392d09c1f5b9c443a53041e67867
gads06121994 <gads06121994@yandex.ru>
2017-01-01 23:59:18 +0300

    [exception] Добавил обработку исключений на глобальном уровне.

--- NumericalMethod/Main.cpp
+++ NumericalMethod/Main.cpp
@@ -11,47 +11,57 @@ int main(int cmdn, char* cmd[])
 {
 	MPI_Init(&cmdn, &cmd);
 
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	try
+	{
 
-	std::cout << "Process " << rank << ", total proc count: " << size << std::endl;
+		int rank, size;
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-
-	InputSet is;
-
-	//---------- - Model Constants--------
-	is.sm1 = 0.04, is.sm2 = 0.06;
-	is.b1 = 0.4; is.b2 = 0.4;
-	is.d11 = 0.001;  is.d22 = 0.001; is.d12 = 0.001; is.d21 = 0.001;
-	is.sw11 = 0.04, is.sw22 = 0.04, is.sw12 = 0.04, is.sw21 = 0.04;
-	is.d1 = 0.2; is.d2 = 0.2;
-
-	//----------Grid settings----------
-	is.N = 256;
-	is.A = 1;
-
-	//-------- - Numerical method Constants------ -
-	is.max_iter = 500;
-
-	//-------- - Closure Constants------ -
-	is.a = 0.4;
+		std::cout << "Process " << rank << ", total proc count: " << size << std::endl;
 
 
+		InputSet is;
 
-	std::vector<InputSet> input;
+		//---------- - Model Constants--------
+		is.sm1 = 0.04, is.sm2 = 0.06;
+		is.b1 = 0.4; is.b2 = 0.4;
+		is.d11 = 0.001;  is.d22 = 0.001; is.d12 = 0.001; is.d21 = 0.001;
+		is.sw11 = 0.04, is.sw22 = 0.04, is.sw12 = 0.04, is.sw21 = 0.04;
+		is.d1 = 0.2; is.d2 = 0.2;
+
+		//----------Grid settings----------
+		is.N = 256;
+		is.A = 1;
+
+		//-------- - Numerical method Constants------ -
+		is.max_iter = 500;
+
+		//-------- - Closure Constants------ -
+		is.a = 0.4;
 
 
-	for (num d12 = 0; d12 < 0.001; d12 += 0.001 * 0.01)
-		for (num sm2 = 0; sm2 < 0.16; sm2 += 0.16 * 0.01)
-		{
-			is.d12 = d12;
-			is.sm2 = sm2;
-			input.push_back(is);
-		}
+
+		std::vector<InputSet> input;
 
 
-	count_input(input);
+		for (num d12 = 0; d12 < 0.001; d12 += 0.001 * 0.01)
+			for (num sm2 = 0; sm2 < 0.16; sm2 += 0.16 * 0.01)
+			{
+				is.d12 = d12;
+				is.sm2 = sm2;
+				input.push_back(is);
+			}
+
+
+		count_input(input);
+
+	}
+	catch (std::exception e)
+	{
+		std::cout << "Unhandled exception: " << e.what() << "\n";
+	}
+
 
 	MPI_Finalize();
 	return 0;
