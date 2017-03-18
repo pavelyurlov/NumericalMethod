3c40baf5ff976d794ace83b8d0f7c845ac64d95b
gads06121994 <gads06121994@yandex.ru>
2017-03-18 05:01:39 +0300

    [Distribution1D] Создал основу.

---
+++ Numerical_Lib/Distribution1D.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "minitypes.h"
+
+class Distribution1D
+{
+public:
+	Distribution1D(/*part,*/ num); // TODO: сделать класс разбиений
+	Distribution1D(/*part, func*/); // TODO: сделать класс функций
+};
